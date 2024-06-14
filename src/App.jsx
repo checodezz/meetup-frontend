294e87887bf1ec4828fb4c8db195c08d11f397da
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-14 22:16:16 +0530

    Add : hero component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+const Hero = () => {
+  return (
+    <div className="container row p-5 m-5">
+      <div className="mb-5 col-md-8 px-5 container">
+        <h1 className="fw-bold  display-5 pb-3">
+          The people platform—Where interests become friendships
+        </h1>
+        <p className="fs-5 text-body-secondary">
+          Whatever your interest, from hiking and reading to networking and
+          skill sharing, there are thousands of people who share it on Meetup.
+          Events are happening every day—sign up to join the fun.
+        </p>
+        <button className="btn text-light" style={{ background: "#00798a" }}>
+          Join Meetup
+        </button>
+      </div>
+      <div className="col-md-4 container">
+        <img
+          src="https://secure.meetupstatic.com/next/images/indexPage/irl_event.svg?w=384"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
