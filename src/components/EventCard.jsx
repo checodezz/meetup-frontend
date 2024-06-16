b00cd809fdfda36964e9fc2531d5c3249fe544aa
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-06-16 13:05:32 +0530

    Add : filter by event type

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -7,15 +7,26 @@ const EventCard = () => {
   const { data, loading, error } = useFetch("http://localhost:3000/event");
 
   const [listOfEvents, setListOfEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
 
   useEffect(() => {
     if (data && data.events.length > 0) {
       setListOfEvents(data.events);
+      setFilteredEvents(data.events);
     }
   }, [data]);
 
   const formatDate = (dateString) => {
     return format(new Date(dateString), "MMMM d, yyyy h:mm a");
+  };
+
+  const handleEventChange = (e) => {
+    const { value } = e.target;
+    value === "all"
+      ? listOfEvents
+      : setFilteredEvents(
+          listOfEvents.filter((event) => event?.eventType == value)
+        );
   };
 
   return (
@@ -25,15 +36,15 @@ const EventCard = () => {
           <h1>Meetup Events</h1>
         </div>
         <div className="col-md-2 text-md-end">
-          <select className="form-select">
-            <option defaultValue>Select Event Type</option>
-            <option value="offline">Online</option>
-            <option value="online">Offline Type 2</option>
+          <select className="form-select" onChange={handleEventChange}>
+            <option value="all">Select Event Type</option>
+            <option value="online">Online</option>
+            <option value="offline">Offline</option>
           </select>
         </div>
       </div>
       <div className="row">
-        {listOfEvents?.map((event) => {
+        {filteredEvents?.map((event) => {
           return (
             <div className="col-md-3 mb-3" key={event._id}>
               <div className="card border-0 card-border">
